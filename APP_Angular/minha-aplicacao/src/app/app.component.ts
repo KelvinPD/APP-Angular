1f9b44325a4ddf5bccdf0b375034b37c3fe090fe
Kelvin <kelvinpimentadias2@gmail.com>
2025-03-22 11:17:52 -0300

    adição de campo cpf e pequenas mudanças graficas V25.03.22

--- APP_Angular/minha-aplicacao/src/app/app.component.ts
+++ APP_Angular/minha-aplicacao/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common'; // Import necessário para *ngFor e *ngIf
+import { CommonModule } from '@angular/common';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
@@ -13,21 +13,33 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 })
 export class AppComponent {
   contacts = [
-    { name: 'João Silva', phone: '(11) 98765-4321' },
-    { name: 'Maria Santos', phone: '(11) 91234-5678' }
+    { name: 'João Silva', phone: '(11) 98765-4321', cpf: '123.456.789-01' },
+    { name: 'Maria Santos', phone: '(11) 91234-5678', cpf: '987.654.321-00' }
   ];
+  
 
   newContactName = '';
   newContactPhone = '';
+  newCPF = '';
+
+  message: string = '';
 
   addContact() {
-    if (this.newContactName.trim() && this.newContactPhone.trim()) {
-      this.contacts.push({ name: this.newContactName.trim(), phone: this.newContactPhone.trim() });
+    if (this.newContactName.trim() && this.newContactPhone.trim() && this.newCPF.trim()) {
+      this.contacts.push({
+        name: this.newContactName.trim(),
+        phone: this.newContactPhone.trim(),
+        cpf: this.newCPF.trim()
+      });
       this.newContactName = '';
       this.newContactPhone = '';
+      this.newCPF = '';
+  
+      this.message = 'Contato adicionado com sucesso!';
+      setTimeout(() => (this.message = ''), 3000);
     }
   }
-
+  
   removeContact(index: number) {
     this.contacts.splice(index, 1);
   }
